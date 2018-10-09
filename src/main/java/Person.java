aa3d73b3dc187b16b3c450708711f7fd579dc6c4
tom <tom@sina.com>
2018-10-09 23:31:16 +0800

    a

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,8 @@
 public class Person {
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+
+    }
+    public void test(){
+        
     }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,6 +1,6 @@
 public class Student {
 
   public static  void main (String[] agrs){
-      System.out.println("hello");
+
   }
 }
